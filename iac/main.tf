a51645b8cdfdffb09083915b31c87945d9d2d907
Andre <andrevitorlopes@gmail.com>
2023-11-28 22:38:04 +0100

    Add file

--- iac/main.tf
+++ iac/main.tf
@@ -17,10 +17,20 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda.js"
+  output_path = "lambda_function_payload.zip"
+}
+
 resource "aws_lambda_function" "get_movies" {
+  filename      = "lambda_function_payload.zip"
   function_name = "GetMoviesLambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs20.x"
 
