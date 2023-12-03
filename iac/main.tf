8d21aa3dfb7ef33760307fc39d844b7f9760966f
Andre <andrevitorlopes@gmail.com>
2023-12-03 19:33:01 +0100

    Add correct resources

--- iac/main.tf
+++ iac/main.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = module.get_movie_lambda.name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:eu-central-1:044256433832:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.movie_get_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn    = "arn:aws:execute-api:eu-central-1:044256433832:${aws_api_gateway_rest_api.movies_api.id}/*/${aws_api_gateway_method.movie_get_method.http_method}${aws_api_gateway_resource.movies_resource.path}"
 }
 
 resource "aws_api_gateway_rest_api" "movies_api" {
