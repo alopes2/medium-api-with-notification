eda193126b9bd2d70090ab033c60556dce8ed8c3
Andre <andrevitorlopes@gmail.com>
2023-12-06 20:12:36 +0100

    Add seed data

--- iac/dynamodb.tf
+++ iac/dynamodb.tf
@@ -16,3 +16,83 @@ resource "aws_dynamodb_table" "movies-table" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table_item" "the_matrix" {
+  table_name = aws_dynamodb_table.movies-table.name
+  hash_key   = aws_dynamodb_table.movies-table.hash_key
+  range_key  = aws_dynamodb_table.movies-table.range_key
+
+  item = <<ITEM
+  {
+    "ID": {"S": "1"},
+    "Title": {"S": "The Matrix"},
+    "Genres": {"SS": [
+        "Action",
+        "Sci-Fi"
+      ]
+    },
+    "Rating": {"N": "8.7"}
+  }
+  ITEM
+}
+
+resource "aws_dynamodb_table_item" "scott_pilgrim" {
+  table_name = aws_dynamodb_table.movies-table.name
+  hash_key   = aws_dynamodb_table.movies-table.hash_key
+  range_key  = aws_dynamodb_table.movies-table.range_key
+
+  item = <<ITEM
+  {
+    "ID": {"S": "2"},
+    "Title": {"S": "Scott Pilgrim vs. the World"},
+    "Genres": {"SS": [
+          "Action",
+          "Comedy"
+        ]
+      },
+    "Rating": {"N": "7.5"}
+  }
+  ITEM
+}
+
+resource "aws_dynamodb_table_item" "star_wars" {
+  table_name = aws_dynamodb_table.movies-table.name
+  hash_key   = aws_dynamodb_table.movies-table.hash_key
+  range_key  = aws_dynamodb_table.movies-table.range_key
+
+  item = <<ITEM
+  {
+    "ID": {"S": "3"},
+    "Title": {"S": "Star Wars: Episode IV - A New Hope"},
+    "Genres": {"SS": [
+          "Action",
+          "Adventure",
+          "Sci-Fi",
+          "Fantasy"
+        ]
+      },
+    "Rating": {"N": "8.6" }
+  }
+  ITEM
+}
+
+resource "aws_dynamodb_table_item" "star_wars_v" {
+  table_name = aws_dynamodb_table.movies-table.name
+  hash_key   = aws_dynamodb_table.movies-table.hash_key
+  range_key  = aws_dynamodb_table.movies-table.range_key
+
+  item = <<ITEM
+  {
+    "ID": {"S": "4"},
+    "Title": {"S": "Star Wars: Episode V - The Empire Strikes Back"},
+    "Genres": {"SS": [
+          "Action",
+          "Adventure",
+          "Sci-Fi",
+          "Fantasy"
+        ]
+      },
+    "Rating": {"N": "8.7" }
+  }
+  ITEM
+}
