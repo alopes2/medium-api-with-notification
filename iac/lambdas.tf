b61bd223864fd830cef9b0d4151e58eab9c9a9b4
Andre <andrevitorlopes@gmail.com>
2023-12-07 16:41:38 +0100

    Add logging to lambda

--- iac/lambdas.tf
+++ iac/lambdas.tf
@@ -5,3 +5,29 @@ module "get_movie_lambda" {
   runtime = "nodejs20.x"
   handler = "index.handler"
 }
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = module.get_movie_lambda.role_name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
