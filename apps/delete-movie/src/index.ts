568a8f0da6743c52adbbac12e086169975abe5a9
Andre <andrevitorlopes@gmail.com>
2023-12-11 14:32:18 +0100

    Publish MovieDeleted event to SNS

--- apps/delete-movie/src/index.ts
+++ apps/delete-movie/src/index.ts
@@ -1,6 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import { MovieDeleted } from "./models.js";
 
 const tableName = "Movies";
 
@@ -31,6 +33,8 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
   try {
     await docClient.send(command);
 
+    await publishEventToSNS(movieID);
+
     return {
       statusCode: 204,
       body: JSON.stringify({
@@ -48,3 +52,30 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     };
   }
 };
+
+async function publishEventToSNS(movieID: string) {
+  const snsClient = new SNSClient({});
+
+  const event: MovieDeleted = {
+    id: movieID,
+  };
+
+  const eventName = "MovieDeleted";
+
+  try {
+    await snsClient.send(
+      new PublishCommand({
+        Message: JSON.stringify(event),
+        TopicArn: "arn:aws:sns:eu-central-1:044256433832:movie-updates-topic",
+        MessageAttributes: {
+          Type: {
+            DataType: "String",
+            StringValue: eventName,
+          },
+        },
+      })
+    );
+  } catch (e: any) {
+    console.warn(e);
+  }
+}
---
+++ apps/delete-movie/src/models.ts
@@ -0,0 +1,3 @@
+export type MovieDeleted = {
+  id: string;
+};
