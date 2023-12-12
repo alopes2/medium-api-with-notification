7644d0ed8e7b18a251a1e7b461a14093cba90b93
Andre Lopes <andrevitorlopes@gmail.com>
2023-12-12 16:42:37 +0100

    Add code to send email

--- apps/email-notification/src/index.ts
+++ apps/email-notification/src/index.ts
@@ -1,19 +1,53 @@
 import { SQSEvent, Context, SQSHandler, SQSRecord } from "aws-lambda";
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 export const handler: SQSHandler = async (event: SQSEvent, context: Context): Promise<void> => {
+  const client = new SESClient({});
+  const promises: Promise<void>[] = [];
   for (const message of event.Records) {
-    await processMessageAsync(message);
+    promises.push(processMessageAsync(message, client));
   }
+
+  await Promise.all(promises);
+
   console.info("done");
 };
 
-async function processMessageAsync(message: SQSRecord): Promise<any> {
+async function processMessageAsync(message: SQSRecord, client: SESClient): Promise<void> {
   try {
-    console.log(`Processed ${message.messageAttributes["Type"].stringValue} message ${message.body}`);
-    // TODO: Do interesting work based on the new message
-    await Promise.resolve(1); //Placeholder for actual async work
+    const eventType = message.messageAttributes["Type"].stringValue ?? "MovieEvent";
+    console.log(`Processing ${eventType} message ${message.body}`);
+
+    const sourceEmail = process.env.SOURCE_EMAIL || ""; // Ideally it needs to be validated and logged if not set
+    const destinationEmail = process.env.DESTINATION_EMAIL || ""; // Ideally it needs to be validated and logged if not set
+
+    console.log(sourceEmail)
+    console.log(destinationEmail)
+
+    const command = new SendEmailCommand({
+      Source: sourceEmail,
+      Destination: {
+        ToAddresses: [destinationEmail]
+      },
+      Message: {
+        Body: {
+          Text: {
+            "Charset": "UTF-8",
+            "Data": message.body
+          }
+        },
+        Subject: {
+          "Charset": "UTF-8",
+          "Data": `${eventType.replace(/([a-z])([A-Z])/, '$1 $2')}`
+        }
+      }
+    });
+
+    await client.send(command);
+
+    console.log(`Processed ${eventType} message ${message.body}`);
   } catch (err) {
     console.error("An error occurred");
-    throw err;
+    console.error(err);
   }
 }
